cdafe678d1336899d20f191c13a3c494ef65dc78
sadjad <sghgh1996@yahoo.com>
2018-03-17 14:57:38 +0330

    apis for groups management done

--- express/index.js
+++ express/index.js
@@ -1,9 +1,13 @@
 module.exports = () => {
     var app = require('express')();
+    var bodyParser = require('body-parser');
     var http = require('http').Server(app);
     var request = require('request');
     var port = 8080;
     var fs = require('fs');
+
+    app.use(bodyParser.json()); // for parsing application/json
+    app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
     const checkReservedUrl = (url)=>{
         let reservedUrl = [
@@ -21,10 +25,28 @@ module.exports = () => {
         reservedUrl.map(function (u) {
             if(url.includes(u)) result = false;
         });
-
         return result;
     };
-
+    
+    const getGroupId = (groups)=>{
+        let maxId = 1;
+        groups.map(function (g) {
+            if(g.id > maxId){
+                maxId = g.id;
+            }
+        });
+        return maxId + 1;
+    };
+    const getWebsiteId = (websites)=>{
+        let maxId = 1;
+        websites.map(function (w) {
+            if(w.id > maxId){
+                maxId = w.id;
+            }
+        });
+        return maxId + 1;
+    };
+    
     http.listen(port, function () {
         console.log('listening on *:' + port);
     });
@@ -55,8 +77,153 @@ module.exports = () => {
             if(err) console.log(err);
             let groups = JSON.parse(buf.toString()).groups;
             resp.send(JSON.stringify(groups));
-            
-            // resp.send('<h1>Hello world</h1>');
+        });
+    });
+    app.get('/get_websites/:group_id', function (req, resp) {
+        fs.readFile('./express/groups.txt', function(err, buf) {
+            if(err) console.log(err);
+            let result = [];
+            let websites = JSON.parse(buf.toString()).websites;
+            websites.map(function (w) {
+                if(w.group_id === parseInt(req.params.group_id, 10)){
+                    result.push(w);
+                }
+            });
+            resp.send(JSON.stringify(result));
+        });
+    });
+    app.get('/get_website/:id', function (req, resp) {
+        fs.readFile('./express/groups.txt', function(err, buf) {
+            if(err) console.log(err);
+            let result = {};
+            let websites = JSON.parse(buf.toString()).websites;
+            websites.map(function (w) {
+                if(w.id === parseInt(req.params.id, 10)){
+                    result = w;
+                }
+            });
+            resp.send(JSON.stringify(result));
+        });
+    });
+    
+    app.post('/add_group', function (req, resp) {
+        fs.readFile('./express/groups.txt', function(err, buf) {
+            if(err) console.log(err);
+            let websites = JSON.parse(buf.toString()).websites;
+            let groups = JSON.parse(buf.toString()).groups;
+            groups.push({
+                name: req.body.name,
+                id:getGroupId(groups)
+            });
+            let obj = {
+                groups: groups,
+                websites: websites
+            };
+            fs.writeFile('./express/groups.txt', JSON.stringify(obj), function(err, data){
+                if(err) console.log(err);
+                resp.send(JSON.stringify({msg: 'ok'}));
+            });
+        });
+    });
+
+    app.post('/add_website', function (req, resp) {
+        fs.readFile('./express/groups.txt', function(err, buf) {
+            if(err) console.log(err);
+            let websites = JSON.parse(buf.toString()).websites;
+            let groups = JSON.parse(buf.toString()).groups;
+            websites.push({
+                id:getWebsiteId(websites),
+                address: req.body.address,
+                group_id: req.body.group_id,
+                dataReceived: 0
+            });
+            let obj = {
+                groups: groups,
+                websites: websites
+            };
+            fs.writeFile('./express/groups.txt', JSON.stringify(obj), function(err, data){
+                if(err) console.log(err);
+                resp.send(JSON.stringify({msg: 'ok'}));
+            });
+        });
+    });
+    app.delete('/delete_website/:id', function (req, resp) {
+        fs.readFile('./express/groups.txt', function(err, buf) {
+            if(err) console.log(err);
+            let websites = JSON.parse(buf.toString()).websites;
+            let groups = JSON.parse(buf.toString()).groups;
+            let removedWebsites = websites.filter(function(el) {
+                return el.id !== parseInt(req.params.id, 10);
+            });
+
+            let obj = {
+                groups: groups,
+                websites: removedWebsites
+            };
+            fs.writeFile('./express/groups.txt', JSON.stringify(obj), function(err, data){
+                if(err) console.log(err);
+                resp.send(JSON.stringify({msg: 'ok'}));
+            });
+        });
+    });
+    app.delete('/delete_group/:id', function (req, resp) {
+        fs.readFile('./express/groups.txt', function(err, buf) {
+            if(err) console.log(err);
+            let websites = JSON.parse(buf.toString()).websites;
+            let groups = JSON.parse(buf.toString()).groups;
+            let removedGroups = groups.filter(function(el) {
+                return el.id !== parseInt(req.params.id, 10);
+            });
+
+            let obj = {
+                groups: removedGroups,
+                websites: websites
+            };
+            fs.writeFile('./express/groups.txt', JSON.stringify(obj), function(err, data){
+                if(err) console.log(err);
+                resp.send(JSON.stringify({msg: 'ok'}));
+            });
+        });
+    });
+    app.put('/edit_website', function (req, resp) {
+        fs.readFile('./express/groups.txt', function(err, buf) {
+            if(err) console.log(err);
+            let websites = JSON.parse(buf.toString()).websites;
+            let groups = JSON.parse(buf.toString()).groups;
+            websites.map(function (w) {
+                if(w.id === parseInt(req.body.id, 10)){
+                    w.address = req.body.address;
+                    w.group_id = req.body.group_id;
+                }
+            });
+            let obj = {
+                groups: groups,
+                websites: websites
+            };
+            fs.writeFile('./express/groups.txt', JSON.stringify(obj), function(err, data){
+                if(err) console.log(err);
+                resp.send(JSON.stringify({msg: 'ok'}));
+            });
+        });
+    });
+    app.put('/edit_group', function (req, resp) {
+        fs.readFile('./express/groups.txt', function(err, buf) {
+            if(err) console.log(err);
+            let websites = JSON.parse(buf.toString()).websites;
+            let groups = JSON.parse(buf.toString()).groups;
+            groups.map(function (g) {
+                if(g.id === parseInt(req.body.id, 10)){
+                    g.name = req.body.name;
+                }
+            });
+            let obj = {
+                groups: groups,
+                websites: websites
+            };
+            fs.writeFile('./express/groups.txt', JSON.stringify(obj), function(err, data){
+                if(err) console.log(err);
+                resp.send(JSON.stringify({msg: 'ok'}));
+            });
         });
     });
 };
